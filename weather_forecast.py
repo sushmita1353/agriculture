fd54af0a0ebf22892529fa0690071be37cb307bc
sushmita1353 <103933243+sushmita1353@users.noreply.github.com>
2024-08-19 22:34:09 +0530

    weather_forecast.py

--- weather_forecast.py
+++ weather_forecast.py
@@ -1,5 +1,4 @@
 def weather_forecast():
-    # Example weather data (static for simplicity)
     print("\nFetching weather forecast for your region...\n")
     print("Today's Weather:")
     print("- Temperature: 28°C")
