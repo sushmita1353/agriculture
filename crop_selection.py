7e7a80e5c037e596ebcb5aed567d64d09b3eb239
sushmita1353 <103933243+sushmita1353@users.noreply.github.com>
2024-08-19 22:35:20 +0530

    crop_selection.py

--- crop_selection.py
+++ crop_selection.py
@@ -2,7 +2,7 @@ def crop_selection():
     region = input("Enter your region (e.g., North, South, East, West): ")
     soil_type = input("Enter the type of soil (e.g., Sandy, Clay, Loamy): ")
 
-    # Example logic for crop recommendation based on region and soil type
+  
     if region.lower() == "south" and soil_type.lower() == "loamy":
         print("\nRecommended Crops for your region and soil type:")
         print("1. Wheat")
