220af7fe266272e51c3f242469db19f762736b3a
sushmita1353 <103933243+sushmita1353@users.noreply.github.com>
2024-08-19 23:08:00 +0530

    Delete sustainable_farming_tips.py

--- sustainable_farming_tips.py
+++
@@ -1,6 +0,0 @@
-def sustainable_farming_tips():
-    print("\nSustainable Farming Tips:")
-    print("1. Practice crop rotation to prevent soil depletion.")
-    print("2. Use organic fertilizers to maintain soil health.")
-    print("3. Implement drip irrigation to conserve water.")
-    print("\n------------------------------------------------")
